ccfc847b4695e81042f1a7b0fe8853d0823d78c4
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2023-12-05 00:32:51 -0300

    Desafio 01
    Add two numbers

---
+++ src/AddTwoIntergers.java
@@ -0,0 +1,41 @@
+
+class Solution {
+    // Add two integers
+    public int sum(int num1, int num2) {
+        // Add two integers
+        int ans = num1+num2;
+        // Return the sum
+        return ans;
+    }
+}
+
+                    //Descrição do codigo //
+/* Este codigo foi feito para resolver um problema de dificuldade nivel : Facil
+*  Classificada como uma Big O de O(1) Tal codigo mostra que indenpendente
+* Do valor atribuido as variaveis Num 01 e Num 02, o tempo de execução desse
+*  método sum permanece constante. Isso ocorre porque a operação realizada dentro
+*  do método (int ans = num1 + num2;) é uma operação de soma simples */
+
+// Link: https://leetcode.com/problems/add-two-numbers/
+
+                        // Desafio
+/*  Given two integers num1 and num2, return the sum of the two integers.
+
+
+        Example 1:
+
+        Input: num1 = 12, num2 = 5
+        Output: 17
+        Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+        Example 2:
+
+        Input: num1 = -10, num2 = 4
+        Output: -6
+        Explanation: num1 + num2 = -6, so -6 is returned.
+
+
+        Constraints:
+
+        -100 <= num1, num2 <= 100
+
+ */
